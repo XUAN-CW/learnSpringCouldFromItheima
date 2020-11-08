b01657cd62822f0fa080cb11bb4caca232cd1e69
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-11-08 22:40:40 +0800

    学习了 LocalFilter

---
+++ 06-gateway/code/heima-gateway/src/main/java/com/itheima/gateway/filter/MyParamGatewayFilterFactory.java
@@ -0,0 +1,58 @@
+package com.itheima.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author XUAN
+ * @date 2020/11/8 - 22:20
+ * @references
+ * @purpose 该过滤器可以在控制台输出配置文件中指定名称的请求参数的值
+ * @errors
+ */
+@Component
+public class MyParamGatewayFilterFactory extends AbstractGatewayFilterFactory<MyParamGatewayFilterFactory.Config> {
+
+    //
+    static final String PARAM_NAME = "param";
+
+    public MyParamGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList(PARAM_NAME);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            // http://localhost:10010/user/8?name=itcast   config.param ==> name
+            //获取请求参数中param对应的参数名 的参数值
+            ServerHttpRequest request = exchange.getRequest();
+            if(request.getQueryParams().containsKey(config.param)){
+                request.getQueryParams().get(config.param).
+                        forEach(value -> System.out.printf("------------局部过滤器--------%s = %s------", config.param, value));
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+    public static class Config{
+        //对应在配置过滤器的时候指定的参数名
+        private String param;
+
+        public String getParam() {
+            return param;
+        }
+
+        public void setParam(String param) {
+            this.param = param;
+        }
+    }
+}
